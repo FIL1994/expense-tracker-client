d6268e4c73e4d57ec3eeced6b69595b9f6c08665
Philip Van Raalte <phil.vanraalte@gmail.com>
2019-04-15 20:12:00 -0400

    added pie chart

--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -1,29 +1,33 @@
 import React from "react";
 import { useExpenses } from "../../hooks/data";
 import { Segment, Header, Card } from "semantic-ui-react";
+import ExpenseChart from "./ExpenseChart";
 
 const Expenses = () => {
   const expenses = useExpenses();
 
   return (
-    <Segment>
-      <Header>Expenses</Header>
-      <Card.Group>
-        {expenses.map(expense => (
-          <Card key={expense.id}>
-            <Card.Content>
-              <Card.Header>{expense.description}</Card.Header>
-              <Card.Description>
-                {Intl.NumberFormat("en-CA", {
-                  style: "currency",
-                  currency: "CAD"
-                }).format(expense.amount)}
-              </Card.Description>
-            </Card.Content>
-          </Card>
-        ))}
-      </Card.Group>
-    </Segment>
+    <>
+      <Segment>
+        <Header>Expenses</Header>
+        <Card.Group>
+          {expenses.map(expense => (
+            <Card key={expense.id}>
+              <Card.Content>
+                <Card.Header>{expense.description}</Card.Header>
+                <Card.Description>
+                  {Intl.NumberFormat("en-CA", {
+                    style: "currency",
+                    currency: "CAD"
+                  }).format(expense.amount)}
+                </Card.Description>
+              </Card.Content>
+            </Card>
+          ))}
+        </Card.Group>
+      </Segment>
+      <ExpenseChart expenses={expenses} />
+    </>
   );
 };
 
