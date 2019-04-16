305e60dcba63dae9d1871214bda5676630c313e2
Philip Van Raalte <phil.vanraalte@gmail.com>
2019-04-15 20:13:25 -0400

    added pie chart (#13)

---
+++ src/components/expenses/ExpenseChart.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { VictoryPie } from "victory";
+
+const ExpenseChart = props => {
+  const { expenses } = props;
+
+  return (
+    <div style={{ height: "40vh" }}>
+      <VictoryPie
+        x="description"
+        y="amount"
+        data={expenses}
+        colorScale={["#DB0010", "#E16A00", "#027E89", "#19BA00"]}
+        events={[
+          {
+            target: "data",
+            eventHandlers: {
+              onMouseEnter: () => [
+                {
+                  target: "data",
+                  mutation: props => ({
+                    style: {
+                      ...props.style,
+                      stroke: "black",
+                      strokeWidth: 2
+                    }
+                  })
+                }
+              ],
+              onMouseLeave: () => [
+                {
+                  target: "data",
+                  mutation: props => ({
+                    style: {
+                      ...props.style,
+                      strokeWidth: 0
+                    }
+                  })
+                }
+              ]
+            }
+          }
+        ]}
+      />
+    </div>
+  );
+};
+
+export default ExpenseChart;
